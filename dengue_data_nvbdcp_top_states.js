45fd087e8879dd59386e791b46b344fd167459b0
abhayjadhav1234567 <anj4591217@gmail.com>
2025-10-04 15:52:12 +0530

    Delete dengue_data_nvbdcp_top_states.js

--- dengue_data_nvbdcp_top_states.js
+++
@@ -1,7 +0,0 @@
-const dengueData = [
-  { state: "Maharashtra", cases: 12000 },
-  { state: "Delhi", cases: 8500 },
-  { state: "Karnataka", cases: 6400 },
-  { state: "Tamil Nadu", cases: 5000 },
-  { state: "Uttar Pradesh", cases: 7200 }
-];
