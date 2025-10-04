bf952ef86dadf9d8f417b73c559f5f25b76a914e
abhayjadhav1234567 <anj4591217@gmail.com>
2025-10-04 14:34:06 +0530

    Delete dengue_data_nvbdcp_top_states.js

--- dengue_data_nvbdcp_top_states.js
+++
@@ -1,6 +0,0 @@
-window.dengueStateData = [
-  {state: "Maharashtra", lat: 19.7515, lng: 75.7139,
-   values: {2021:{cases:1000,deaths:5},2022:{cases:2000,deaths:10},2023:{cases:1500,deaths:7},2024:{cases:2500,deaths:12},2025:{cases:3000,deaths:15}}},
-  {state: "Delhi", lat: 28.7041, lng: 77.1025,
-   values: {2021:{cases:500,deaths:3},2022:{cases:800,deaths:5},2023:{cases:600,deaths:4},2024:{cases:1200,deaths:6},2025:{cases:1500,deaths:8}}}
-];
